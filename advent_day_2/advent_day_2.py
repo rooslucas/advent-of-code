234d26909da68b755f8b11473efb9d732b9b989d
rooslucas <rooslucas@live.nl>
2020-12-02 16:06:14 +0100

    Refactor PasswordCheck

--- advent_day_2/advent_day_2.py
+++ advent_day_2/advent_day_2.py
@@ -7,14 +7,19 @@ password_checks = []
 
 
 class PasswordCheck:
-    def __init__(self, policy, password):
-        self.policy = policy
+    def __init__(self, password, min_count, max_count, letter):
         self.password = password
+        self.min_count = min_count
+        self.max_count = max_count
+        self.letter = letter
 
 
+# Parse input into PasswordChecks
 for line in advent_input:
-    (policy, password) = line.strip().split(':')
-    password_checks.append(PasswordCheck(policy, password))
+    policy, password = line.strip().split(':')
+    number, letter = policy.split(' ')
+    min_count, max_count = number.split('-')
+    password_checks.append(PasswordCheck(password, min_count, max_count, letter))
 
 advent_input.close()
 
@@ -22,11 +27,9 @@ advent_input.close()
 correct_passwords = 0
 
 for password_check in password_checks:
-    number, letter = password_check.policy.split(' ')
-    letter_count = password_check.password.count(letter)
-    min_count, max_count = number.split('-')
+    letter_count = password_check.password.count(password_check.letter)
 
-    if int(min_count) <= letter_count <= int(max_count):
+    if int(password_check.min_count) <= letter_count <= int(password_check.max_count):
         correct_passwords += 1
 
 print(correct_passwords)
