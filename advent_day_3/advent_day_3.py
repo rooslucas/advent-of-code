0223f8d11c8ce75a3dd9c63d4e7d1a1ba0e7551c
rooslucas <rooslucas@live.nl>
2020-12-03 22:28:30 +0100

    Add code from day 3 of advent of code

---
+++ advent_day_3/advent_day_3.py
@@ -0,0 +1,39 @@
+# Advent of code day 3
+
+# Get the data
+advent_input = open("input_3.txt")
+advent_data = advent_input.readlines()
+advent_data = [x.rstrip() for x in advent_data]
+
+
+# Create a function that counts the trees
+def check_trees(right, down):
+    line_index = 0
+    word_index = 0
+    trees_list = []
+    while line_index < len(advent_data) - 1:
+        word_index += right
+        word = word_index % len(advent_data[line_index])
+        line_index += down
+        line = advent_data[line_index]
+        obstacle = line[word]
+        if obstacle == '.':
+            obstacle = '0'
+            trees_list.append(obstacle)
+        elif obstacle == '#':
+            obstacle = 'X'
+            trees_list.append(obstacle)
+
+    return trees_list.count('X')
+
+
+# Count the trees for 5 different slopes
+slope_1 = check_trees(1, 1)
+slope_2 = check_trees(3, 1)
+slope_3 = check_trees(5, 1)
+slope_4 = check_trees(7, 1)
+slope_5 = check_trees(1, 2)
+
+# Get the total amount of trees
+total_trees = slope_1 * slope_2 * slope_3 * slope_4 * slope_5
+print(total_trees)
