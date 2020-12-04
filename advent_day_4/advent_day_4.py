8b69e6208919353c7854d2bbd8fbd964b8b5f213
rooslucas <rooslucas@live.nl>
2020-12-04 20:11:27 +0100

    Remove \n characters from passport_list

--- advent_day_4/advent_day_4.py
+++ advent_day_4/advent_day_4.py
@@ -16,8 +16,14 @@ passport_list = []
 
 # Combine the passports
 for enter in enter_list:
-    passport_list.append(''.join(advent_data[start_element:enter]))
+    passport_list.append(' '.join(advent_data[start_element:enter]))
     start_element = enter + 1
+
+print(passport_list)
+
+for word in passport_list:
+    if "\n" in word:
+        passport_list[passport_list.index(word)] = word.replace("\n", ' ')
 
 print(passport_list)
 
@@ -35,13 +41,13 @@ def passport_check_part_one(passport):
 
 
 def passport_check_part_two(passport):
-    byr = re.compile(r'byr:(19[23456789]\d|200[012])')
-    iyr = re.compile(r'iyr:(20[1]\d|2020)')
-    eyr = re.compile(r'eyr:(20[2]\d|2030)')
-    hcl = re.compile(r'hcl:#[0-9a-f]{6}')
-    ecl = re.compile(r'ecl:(amb|blu|brn|gry|grn|hzl|oth)')
-    pid = re.compile(r'pid:\d{9}')
-    hgt = re.compile(r'59in|6\din|7[0123456]in|1[5678]\dcm|19[01234]cm') # TODO: Check hgt prefix
+    byr = re.compile(r'byr:(19[23456789]\d|200[012]) ')
+    iyr = re.compile(r'iyr:(20[1]\d|2020) ')
+    eyr = re.compile(r'eyr:(20[2]\d|2030) ')
+    hcl = re.compile(r'hcl:#[0-9a-f]{6} ')
+    ecl = re.compile(r'ecl:(amb |blu |brn |gry |grn |hzl |oth )')
+    pid = re.compile(r'pid:\d{9} ')
+    hgt = re.compile(r'hgt:(59in |6\din |7[0123456]in |1[5678]\dcm |19[01234]cm )')
 
     passport_regex_list = [byr, iyr, eyr, hcl, ecl, pid, hgt]
     for check in passport_regex_list:
