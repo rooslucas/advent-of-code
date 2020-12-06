372015f6264ac80cb4e39adf9a483bca84a6a73f
rooslucas <rooslucas@live.nl>
2020-12-06 19:27:04 +0100

    Add part two of advent of code day 6

--- advent_day_6/advent_day_6.py
+++ advent_day_6/advent_day_6.py
@@ -1,27 +1,7 @@
 advent_input = open('input_6.txt')
-advent_data = advent_input.readlines()
+advent_data = advent_input.read()
 
-enter_list = []
-start_element = 0
-
-for line in range(len(advent_data)):
-    if advent_data[line] == '\n':
-        enter_list.append(line)
-enter_list.append((advent_data.index(advent_data[-1]) + 1))
-
-question_list = []
-
-# Combine the passports
-for enter in enter_list:
-    question_list.append(''.join(advent_data[start_element:enter]))
-    start_element = enter + 1
-
-
-print(question_list)
-
-for word in question_list:
-    if "\n" in word:
-        question_list[question_list.index(word)] = word.replace("\n", '')
+question_list = [line.split('\n') for line in advent_data.split('\n\n')]
 
 print(question_list)
 
@@ -30,12 +10,29 @@ alfabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
 
 correct_questions = 0
 
-for word in question_list:
+# Part one
+for group in question_list:
     word_correct = 0
+    group = ', '.join(group)
+    print(group)
     for letter in alfabet:
-        if letter in word:
+        if letter in group:
             word_correct += 1
             correct_questions += 1
-        print(word_correct, correct_questions)
 
 print(correct_questions)
+
+# Part two
+total_correct = 0
+
+for group in question_list:
+    for letter in alfabet:
+        letter_count = 0
+        for person in group:
+            if letter in person:
+                letter_count += 1
+
+        if letter_count == len(group):
+            total_correct += 1
+
+print(total_correct)
