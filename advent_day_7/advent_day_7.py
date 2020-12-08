a4faa9efa8c3074bc89f61d23b51f1bc9d126ab5
rooslucas <rooslucas@live.nl>
2020-12-08 21:33:15 +0100

    Add part one of day 7

--- advent_day_7/advent_day_7.py
+++ advent_day_7/advent_day_7.py
@@ -1,7 +1,6 @@
 import re
 
-advent_input = open("text.txt")
-# baggage = [line.split(' bags contain ') for line in advent_data.split('\n')]
+advent_input = open("input_7.txt")
 
 can_be_contained_in = {}
 
@@ -11,7 +10,10 @@ for line in advent_input.readlines():
     baby_shark_regex = re.compile(r'((\w+) (\w+) (bag|bags))')
     baby_shark = baby_shark_regex.findall(baby_sharks)
     for babies in baby_shark:
-        baby_sharkies.append(babies[0].replace(' bags', ''))
+        if 'bags' in babies:
+            baby_sharkies.append(babies[0].replace(' bags', ''))
+        elif 'bag' in babies:
+            baby_sharkies.append(babies[0].replace(' bag', ''))
 
     for baby in baby_sharkies:
         if baby in can_be_contained_in:
@@ -21,3 +23,23 @@ for line in advent_input.readlines():
             can_be_contained_in[baby].append(daddy_or_mommy_shark)
 
 print(can_be_contained_in)
+
+
+containers = []
+new_containers = []
+
+def find_container_for(target):
+    if target in can_be_contained_in:
+        containers_for_target = can_be_contained_in[target]
+        containers.extend(containers_for_target)
+        for container_for_target in containers_for_target:
+            find_container_for(container_for_target)
+
+
+find_container_for("shiny gold")
+
+for container in containers:
+    if container not in new_containers:
+        new_containers.append(container)
+
+print(len(new_containers))
