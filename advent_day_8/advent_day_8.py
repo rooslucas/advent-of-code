3a98ff8c443862c8a5a7ee2099c481b34510e461
rooslucas <rooslucas@live.nl>
2020-12-12 17:42:51 +0100

    Add part two of advent of code day 8

--- advent_day_8/advent_day_8.py
+++ advent_day_8/advent_day_8.py
@@ -18,10 +18,13 @@ advent_input.close()
 index = 0
 accumulator = 0
 first_round = []
+index_list = []
 
+# Part one
 while True:
     instruction = instructions[index]
     if instruction not in first_round:
+        index_list.append(index)
         if instruction.operation == 'acc':
             accumulator += int(instruction.argument)
             index += 1
@@ -36,3 +39,49 @@ while True:
         break
 
 print(accumulator)
+print(len(index_list))
+print(len(first_round))
+
+# Part two
+
+jmp_nop = []
+
+
+for index in index_list:
+    if instructions[index].operation != 'jmp' or instructions[index].operation != 'nop':
+        index_list.remove(index)
+
+print(len(index_list))
+
+for attempt in index_list:
+    while True:
+        index = 0
+        accumulator_2 = 0
+        index_list_2 = []
+        if instructions[attempt].operation == 'jmp':
+            instructions[attempt].operation = 'nop'
+        elif instructions[attempt].operation == 'nop':
+            instructions[attempt].operation = 'jmp'
+        while True:
+            instruction = instructions[index]
+            if index == 612:
+                break
+            elif index in index_list_2:
+                break
+            else:
+                index_list_2.append(index)
+                if instruction.operation == 'acc':
+                    accumulator_2 += int(instruction.argument)
+                    index += 1
+                elif instruction.operation == 'jmp':
+                    index += int(instruction.argument)
+                elif instruction.operation == 'nop':
+                    index += 1
+        if index == 612:
+            break
+        if instructions[attempt].operation == 'jmp':
+            instructions[attempt].operation = 'nop'
+        elif instructions[attempt].operation == 'nop':
+            instructions[attempt].operation = 'jmp'
+
+print(accumulator_2)
